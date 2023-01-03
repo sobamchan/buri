e5bd0f7b9faece25364928b0df77b59b09db88cc
sobamchan <oh.sore.sore.soutarou@gmail.com>
2023-01-03 18:36:21 +0100

    A script to extract and merge s2ocr files.

---
+++ buri/extract.py
@@ -0,0 +1,58 @@
+import gzip
+from tqdm import tqdm
+import json
+import os
+from argparse import ArgumentParser
+
+import sienna
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--data-dir",
+        type=str,
+        required=True,
+        help="Path to S2ORC dataset dir. It should contain two dirs, `metadata` and `pdf_parses`",
+    )
+    parser.add_argument(
+        "--odir", type=str, required=True, help="Path to save generated text file."
+    )
+    parser.add_argument("--final-single-file", type=str, required=False, default=None, help="One large jsonl file contains all records.")
+    args = parser.parse_args()
+
+    # Extract titles
+    print("Extract titiles...")
+    meta_data_dir = os.path.join(args.data_dir, "metadata")
+    for fname in tqdm(os.listdir(meta_data_dir)):
+        fpath = os.path.join(meta_data_dir, fname)
+
+        with gzip.open(fpath) as fp:
+            for line in fp:
+                metadata = json.loads(line.decode("utf-8"))
+                paper_id = metadata["paper_id"]
+                title = metadata["title"]
+                abstract = metadata["abstract"]
+                sienna.save(
+                    {"paper_id": paper_id, "title": title, "abstract": abstract},
+                    os.path.join(args.odir, f"{paper_id}.jsonl"),
+                )
+
+    # Extract main texts and add file created above
+    print("Extract body texts...")
+    pdf_parses_path = os.path.join(args.data_dir, "pdf_parses")
+    for fname in os.listdir(pdf_parses_path):
+        fpath = os.path.join(pdf_parses_path, fname)
+        with gzip.open(fpath) as fp:
+            for line in fp:
+                pdfparses = json.loads(line.decode("utf-8"))
+                paper_id = pdfparses["paper_id"]
+                text = [
+                    f"{body_text['section']} body_text['text']"
+                    for body_text in pdfparses["body_text"]
+                ]
+                record = sienna.load(os.path.join(args.odir, f"{paper_id}.jsonl"))[0]
+                record["body_text"] = text
+
+                if args.final_single_file is not None:
+                    with open(args.final_single_file, "a") as f:
+                        f.write(json.dumps(record) + "\n")
